495b6e0eeb3e2f4f436b9ea57f5c694a5f1e1bc4
Vlad <vladdaking@gmail.com>
2022-02-14 15:36:45 +0300

    init 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActionsComponent } from './actions/actions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActionsComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
